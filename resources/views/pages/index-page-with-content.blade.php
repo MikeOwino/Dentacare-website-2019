ce60614e05d7c93364f05c75587f8c337f6ba1c6
Mediabasket <office@mediabasket.eu>
2020-01-28 14:16:20 +0200

    optimize packages and add cookie

--- resources/views/pages/index-page-with-content.blade.php
+++ resources/views/pages/index-page-with-content.blade.php
@@ -1,6 +1,6 @@
 @extends("layout")
 @section("content")
-    @if(is_object($page) && property_exists($page, 'html') && !empty($page->html))
+    @if(!empty($page->html))
         {{(new \App\Http\Controllers\PagesController())->shortcodeExtractor(html_entity_decode($page->html))}}
     @endif
 @endsection
