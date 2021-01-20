277aceaa5414ef9f620cd38f508cc83dfe36943d
unknown <miroslav.nedelchev@dentacoin.com>
2021-01-20 13:39:09 +0200

    test post method

--- app/Http/Controllers/DentacareDCNController.php
+++ app/Http/Controllers/DentacareDCNController.php
@@ -205,7 +205,6 @@ class DentacareDCNController extends Controller {
         Log::info('doubleCheckDentacareTransaction method:', ['type' => $request->input('type'), 'address' => $request->input('address'), 'amount' => $request->input('amount'), 'transaction_id' => $request->input('transaction_id')]);
 
         if ($request->input('type') == 'dentacare') {
-            Log::info('Dentacare user logged in successfully.');
             $transaction = DB::connection('mysql2')->table('wallet_transactions')->select('wallet_transactions.*')->where(array('wallet_transactions.wallet_dcn' => $request->input('address'), 'wallet_transactions.amount_dcn' => $request->input('amount'), 'wallet_transactions.id' => $request->input('transaction_id')))->get()->first();
             if (!empty($transaction)) {
                 Log::info('doubleCheckDentacareTransaction method succeed.');
@@ -215,6 +214,7 @@ class DentacareDCNController extends Controller {
                 return response()->json(['success' => false]);
             }
         } else {
+            Log::info('doubleCheckDentacareTransaction method wrong type.');
             return response()->json(['success' => false]);
         }
     }
